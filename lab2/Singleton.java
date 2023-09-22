e73ba312ce1bea346c7695c8e3ab3625a7fe33c5
Talah Khan <101322505+determinoX@users.noreply.github.com>
2023-09-22 09:02:24 +0500

    Add files via upload

---
+++ lab2/Singleton.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Singleton;
+
+/**
+ *
+ * @author FA20-BSE-042
+ */
+public class Singleton {
+
+    private static volatile Singleton instance = null;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized(Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
