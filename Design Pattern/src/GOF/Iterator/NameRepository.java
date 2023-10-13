ec38038455fe90d5594141fd698613d681963102
Talah Khan <talahkhan01@yahoo.com>
2023-10-13 09:01:42 +0500

    iterator

---
+++ Design Pattern/src/GOF/Iterator/NameRepository.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GOF.Iterator;
+
+/**
+ *
+ * @author FA20-BSE-042
+ */
+public class NameRepository implements Container {
+   public String names[] = {"Robert" , "John" ,"Julie" , "Lora"};
+
+   @Override
+   public Iterator getIterator() {
+      return new NameIterator();
+   }
+
+   private class NameIterator implements Iterator {
+
+      int index;
+
+      @Override
+      public boolean hasNext() {
+      
+         if(index < names.length){
+            return true;
+         }
+         return false;
+      }
+
+      @Override
+      public Object next() {
+      
+         if(this.hasNext()){
+            return names[index++];
+         }
+         return null;
+      }		
+   }
+}
