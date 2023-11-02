2cdcffa44ef9033cd900863a2a59bdbba0322200
Talah Khan <talahkhan01@yahoo.com>
2023-11-02 11:56:36 +0500

    ok

--- FA23 Midterm Observer/Code/SportsSubsricption.java
+++ FA23 Midterm Observer/Code/SportsSubsricption.java
@@ -1,5 +1,20 @@
-public class SportsSubsricption {
-   void subscribe(){}
-   void unsubscribe(){}
+public interface SportsSubsricption implements Sportsobserver {
+   //method will help to subscribe
+   void subscribe(){
+   System.out.println("subscribe to hockey"); 
+   System.out.println("subscribe to football");
+   System.out.println("subscribe to cricket");
+   System.out.println("subscribe to volleyball");
+   
+   }
+   //method will help to unsubscribe
+   void unsubscribe(){
+   System.out.println("unsubscribe to hockey"); 
+   System.out.println("unsubscribe to football");
+   System.out.println("unsubscribe to cricket");
+   System.out.println("unsubscribe to volleyball");
+   }
+   //will notify the subscribers about updates
    void notifysubscriber(){}
+
 }
--- FA23 Midterm Observer/Code/Sportsobserver.java
+++ FA23 Midterm Observer/Code/Sportsobserver.java
@@ -1,3 +1,17 @@
-public class Sportsobserver {
-    void Updatesportsobserver(){}
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Flow.Subscriber;
+
+public interface Sportsobserver {
+    void subscribe(SportsSubsricption  subscriber);
+    void unsubscribe(SportsSubsricption  subscriber);
+    void notifysubscriber(SportsSubsricption  subscriber);
+}
+//sportssubscription.java
+class SportsSubsricption implements Sportsobserver {
+private List<Subscriber> subscribers = new ArrayList<>();
+ public void sendSportsUpdate(String sport, String message){
+  notifySubscriber(sport,message);
+}
+
 }
