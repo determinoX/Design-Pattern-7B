a9a026645487bb286532a6262fde5056398590c2
Talah Khan <talahkhan01@yahoo.com>
2023-11-10 08:36:04 +0500

    updated

---
+++ Design Pattern/GOF/Creational Pattern/Prototype/LabTask/Circle.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FA20-BSE-042
+ */
+package refactoring_guru.prototype.example.shapes;
+
+public class Circle extends Shape {
+    public int radius;
+
+    public Circle() {
+    }
+
+    public Circle(Circle target) {
+        super(target);
+        if (target != null) {
+            this.radius = target.radius;
+        }
+    }
+
+    @Override
+    public Shape clone() {
+        return new Circle(this);
+    }
+
+    @Override
+    public boolean equals(Object object2) {
+        if (!(object2 instanceof Circle) || !super.equals(object2)) return false;
+        Circle shape2 = (Circle) object2;
+        return shape2.radius == radius;
+    }
+}
