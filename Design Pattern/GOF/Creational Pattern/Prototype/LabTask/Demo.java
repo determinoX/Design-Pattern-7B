aea228d7f7a9de802ae5a22a13df0fcf5f7c2d38
Talah Khan <talahkhan01@yahoo.com>
2023-11-10 08:44:38 +0500

    demo,java

---
+++ Design Pattern/GOF/Creational Pattern/Prototype/LabTask/Demo.java
@@ -0,0 +1,61 @@
+package refactoring_guru.prototype.example;
+
+import refactoring_guru.prototype.example.shapes.Circle;
+import refactoring_guru.prototype.example.shapes.Rectangle;
+import refactoring_guru.prototype.example.shapes.Shape;
+import refactoring_guru.prototype.example.shapes.Square;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+    public static void main(String[] args) {
+        List<Shape> shapes = new ArrayList<>();
+        List<Shape> shapesCopy = new ArrayList<>();
+
+        Circle circle = new Circle();
+        circle.x = 10;
+        circle.y = 20;
+        circle.radius = 15;
+        circle.color = "red";
+        shapes.add(circle);
+
+        Circle anotherCircle = (Circle) circle.clone();
+        shapes.add(anotherCircle);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.width = 10;
+        rectangle.height = 20;
+        rectangle.color = "blue";
+        shapes.add(rectangle);
+
+
+        
+        Square square = new Square();
+        square.width = 10;
+        square.height = 20;
+        square.color = "blue";
+        shapes.add(square);
+
+        cloneAndCompare(shapes, shapesCopy);
+    }
+
+    private static void cloneAndCompare(List<Shape> shapes, List<Shape> shapesCopy) {
+        for (Shape shape : shapes) {
+            shapesCopy.add(shape.clone());
+        }
+
+        for (int i = 0; i < shapes.size(); i++) {
+            if (shapes.get(i) != shapesCopy.get(i)) {
+                System.out.println(i + ": Shapes are different objects (yay!)");
+                if (shapes.get(i).equals(shapesCopy.get(i))) {
+                    System.out.println(i + ": And they are identical (yay!)");
+                } else {
+                    System.out.println(i + ": But they are not identical (booo!)");
+                }
+            } else {
+                System.out.println(i + ": Shape objects are the same (booo!)");
+            }
+        }
+    }
+}
