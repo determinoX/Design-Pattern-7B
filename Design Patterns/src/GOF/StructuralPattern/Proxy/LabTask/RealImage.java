a9ee218f1f03af793a67f57cc5e7e487f2188d7d
Talah Khan <talahkhan01@yahoo.com>
2023-11-17 09:38:53 +0500

    labtask

---
+++ Design Patterns/src/GOF/StructuralPattern/Proxy/LabTask/RealImage.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GOF.StructuralPattern.Proxy.LabTask;
+
+/**
+ *
+ * @author FA20-BSE-042
+ */
+public class RealImage implements Image {
+
+   private String fileName;
+
+    public RealImage(String fileName){
+        this.fileName = fileName;
+        loadFromDisk(fileName);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Displaying " + fileName);
+    }
+
+    private void loadFromDisk(String fileName){
+        System.out.println("Loading " + fileName);
+    }
+}
