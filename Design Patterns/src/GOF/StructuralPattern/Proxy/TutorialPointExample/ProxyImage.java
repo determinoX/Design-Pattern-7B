c2266d1bfd5bf4b8dff6e413d4b3b1f0e6adef13
Talah Khan <talahkhan01@yahoo.com>
2023-11-17 09:36:15 +0500

    k

---
+++ Design Patterns/src/GOF/StructuralPattern/Proxy/TutorialPointExample/ProxyImage.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GOF.StructuralPattern.Proxy.TutorialPointExample;
+
+/**
+ *
+ * @author FA20-BSE-042
+ */
+public class ProxyImage implements Image{
+
+    private RealImage realImage;
+    private String fileName;
+
+    public ProxyImage(String fileName){
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void display() {
+        if(realImage == null){
+            realImage = new RealImage(fileName);
+        }
+        realImage.display();
+    }
+}
---
+++ Design Patterns/src/GOF/StructuralPattern/Proxy/TutorialPointExample/ProxyPatternDemo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GOF.StructuralPattern.Proxy.TutorialPointExample;
+
+/**
+ *
+ * @author FA20-BSE-042
+ */
+public class ProxyPatternDemo {
+	
+    public static void main(String[] args) {
+        Image image = new ProxyImage("test_10mb.jpg");
+
+        //image will be loaded from disk
+        image.display();
+        System.out.println("");
+
+        //image will not be loaded from disk
+        image.display();
+    }
+}
