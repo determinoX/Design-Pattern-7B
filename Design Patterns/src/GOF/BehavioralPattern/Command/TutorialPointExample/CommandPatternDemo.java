6fd1b19a977269119be844bd13b2464409b02822
Talah Khan <talahkhan01@yahoo.com>
2023-11-24 08:41:42 +0500

    commadntpe

---
+++ Design Patterns/src/GOF/BehavioralPattern/Command/TutorialPointExample/CommandPatternDemo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GOF.BehavioralPattern.Command.TutorialPointExample;
+/**
+ *
+ * @author FA20-BSE-042
+ */
+public class CommandPatternDemo {
+   public static void main(String[] args) {
+      Stock abcStock = new Stock();
+
+      BuyStock buyStockOrder = new BuyStock(abcStock);
+      SellStock sellStockOrder = new SellStock(abcStock);
+
+      Broker broker = new Broker();
+      broker.takeOrder(buyStockOrder);
+      broker.takeOrder(sellStockOrder);
+
+      broker.placeOrders();
+   }
+}
