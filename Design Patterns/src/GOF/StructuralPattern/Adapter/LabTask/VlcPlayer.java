bba5555f701dd9218a9a97c8740a68ae7cad9796
Talah Khan <talahkhan01@yahoo.com>
2023-11-24 08:58:46 +0500

    Update VlcPlayer.java

--- Design Patterns/src/GOF/StructuralPattern/Adapter/LabTask/VlcPlayer.java
+++ Design Patterns/src/GOF/StructuralPattern/Adapter/LabTask/VlcPlayer.java
@@ -1,1 +1,12 @@
+package GOF.StructuralPattern.Adapter.LabTask;
+public class VlcPlayer implements AdvancedMediaPlayer{
+   @Override
+   public void playVlc(String fileName) {
+      System.out.println("Playing vlc file. Name: "+ fileName);		
+   }
 
+   @Override
+   public void playMp4(String fileName) {
+      //do nothing
+   }
+}
